97fe7b7c70436efc7f59c1ea1fa278c1d822b7ef
Bianca <biancataisep@gmail.com>
2022-03-08 13:33:31 -0600

    adding new queries and table Employee count by department number (emp_per_dept)

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -35,11 +35,86 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');	
 
---- create a table
-SELECT first_name, last_name
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
+-- Check the table
 SELECT * FROM retirement_info;
+
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+	retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+
+-- Joining retirement_info and dept_emp tables(with nicknames).
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining departments and dept_manager tables (with nicknames).
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+
+--- Joinning retirement_info and dept_emp tables (with nicknames).
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+--- create a new table to hold the information. Let's name it "current_emp."
+INTO current_emp
+--- add the code that will join these two tables.
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+--- Finally, because this is a table of current employees, we need to add 
+---a filter, using the WHERE keyword and the date 9999-01-01.
+WHERE de.to_date = ('9999-01-01');
+--- Display the new table.
+SELECT * FROM current_emp;
+
+
+-- Create a table Employee count by department number (emp_per_dept).
+---The COUNT function was used on the employee numbers.
+---Aliases were assigned to both tables.
+---GROUP BY was added to the SELECT statement.
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO emp_per_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+SELECT * FROM emp_per_dept;
+
+
+
