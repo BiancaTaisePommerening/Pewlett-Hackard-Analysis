78e34556aceffe5f4695c8e0c2c705784d8aafe8
Bianca <biancataisep@gmail.com>
2022-03-09 14:14:51 -0600

    query more info

--- Data/EmployeeDB.sql
+++ Data/EmployeeDB.sql
@@ -1,3 +1,68 @@
+-- Creating tables for PH-EmployeeDB
+
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+
+CREATE TABLE employees (
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+CREATE TABLE salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+	---FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	---FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	---PRIMARY KEY (emp_no)
+);
+	
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL
+	---FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	---PRIMARY KEY (emp_no)
+);
+--- Displaying each table (each line needs to be run separately).
+SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM titles;
+
+
 ---Amount of employees that were born betweenn 1952 and 1955.
 SELECT first_name, last_name
 FROM employees
@@ -157,3 +222,4 @@ INNER JOIN departments as d
 ON (dm.dept_no = d.dept_no)
 INNER JOIN current_emp as ce 
 ON (dm.emp_no = ce.emp_no)
+
